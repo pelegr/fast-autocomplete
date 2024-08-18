74d76ecd098fe0564e41d73ca4323bbb1f1b2900
Peleg Rabinowitz <peleg@dofinity.com>
2024-08-18 14:41:37 +0300

    Fix normalizer class to support unicode properly.

--- fast_autocomplete/normalize.py
+++ fast_autocomplete/normalize.py
@@ -1,6 +1,7 @@
 import string
+import sys
+import unicodedata
 from fast_autocomplete.lfucache import LFUCache
-
 
 NORMALIZED_CACHE_SIZE = 2048
 MAX_WORD_LENGTH = 40
@@ -19,11 +20,13 @@ class Normalizer:
         if valid_chars_for_string:
             self.valid_chars_for_string = frozenset(valid_chars_for_string)
         else:
-            self.valid_chars_for_string = frozenset({i for i in string.ascii_letters.lower()})
+            self.valid_chars_for_string = frozenset(c for c in map(chr, range(sys.maxunicode + 1))
+                                                    if unicodedata.category(c).startswith('L'))
         if valid_chars_for_integer:
             self.valid_chars_for_integer = frozenset(valid_chars_for_integer)
         else:
-            self.valid_chars_for_integer = frozenset({i for i in string.digits})
+            self.valid_chars_for_integer = frozenset(c for c in map(chr, range(sys.maxunicode + 1))
+                                                     if unicodedata.category(c).startswith('N'))
         if valid_chars_for_node_name:
             self.valid_chars_for_node_name = valid_chars_for_node_name
         else:
@@ -35,7 +38,7 @@ class Normalizer:
     def normalize_node_name(self, name, extra_chars=None):
         if name is None:
             return ''
-        name = name[:MAX_WORD_LENGTH]
+        name = Normalizer.normalize_unicode(name)[:MAX_WORD_LENGTH]
         key = name if extra_chars is None else f"{name}{extra_chars}"
         result = _normalized_lfu_cache.get(key)
         if result == -1:
@@ -56,22 +59,30 @@ class Normalizer:
         """
         if name is None:
             return ''
-        name = name.lower()[:MAX_WORD_LENGTH]
+        name = Normalizer.normalize_unicode(name)[:MAX_WORD_LENGTH]
         self.prev_x = ''
 
         return ''.join(filter(self._remove_invalid_chars, name)).strip()
 
     def _get_normalized_node_name(self, name, extra_chars=None):
-        name = name.lower()
+        name = name.casefold()
         result = []
         last_i = None
         for i in name:
             if i in self.valid_chars_for_node_name or (extra_chars and i in extra_chars):
                 if i == '-':
                     i = ' '
-                elif (i in self.valid_chars_for_integer and last_i in self.valid_chars_for_string) or (i in self.valid_chars_for_string and last_i in self.valid_chars_for_integer):
-                    result.append(' ')
+                elif last_i is not None:
+                    i_cat = unicodedata.category(i)
+                    last_i_cat = unicodedata.category(last_i)
+                    if ((i_cat.startswith('N') and last_i_cat.startswith('L')) or
+                        (i_cat.startswith('L') and last_i_cat.startswith('N'))):
+                        result.append(' ')
                 if not(i == last_i == ' '):
                     result.append(i)
                     last_i = i
         return ''.join(result).strip()
+
+    @staticmethod
+    def normalize_unicode(text):
+        return unicodedata.normalize('NFKC', text)
